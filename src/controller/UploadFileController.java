fc665c5b27bbc56bea2f34ce94b4fd9b18f0a90e
Jin Quan Kang <kjq20000630@126.com>
2023-12-01 15:45:54 +0800

    上传文件

--- src/controller/UploadFileController.java
+++ src/controller/UploadFileController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class UploadFileController
  */
-@WebServlet("/UploadFileController")
+@WebServlet("/uploadfile.do")
 public class UploadFileController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
